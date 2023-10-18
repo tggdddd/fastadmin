7b9d9639588f7449792ab85d4a8b39ed5e4435be
jack <15014586591@139.com>
2023-10-18 10:36:38 +0800

    123

--- bin/deploy.sh
+++
@@ -1,4 +0,0 @@
-path="../public/"
-target="/www/admin/jackr.cn_80/wwwroot"
-rm -r "${target}/*"
-mv ${path}/* "${target}/"
---
+++ bin/webhook.php
@@ -0,0 +1,5 @@
+<?php
+$projectName = "fastadmin";
+$remote = "https://gitee.com/xiao-runjie/fastadmin.git";
+`rm -r $projectName`;
+`git clone $remote`;
