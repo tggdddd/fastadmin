7aa114d258928e6e77be58e0e7fefb6b8a96518d
jack <15014586591@139.com>
2023-11-08 18:49:58 +0800

    RECORD

--- application/config.php
+++ application/config.php
@@ -278,7 +278,7 @@ return [
         //是否同一账号同一时间只能在一个地方登录
         'login_unique'          => false,
         //是否开启IP变动检测
-        'loginip_check'         => true,
+        'loginip_check' => false,
         //登录页默认背景图
         'login_background'      => "",
         //是否启用多级菜单导航
