7472da8f9e0ade04b663d1bd042aff9ee3d6bd63
jack <15014586591@139.com>
2023-11-09 12:35:09 +0800

    333

--- application/common/model/Admin.php
+++ application/common/model/Admin.php
@@ -8,5 +8,15 @@ class Admin extends Model
 {
     // 表名
     protected $name = 'admin';
+    protected $append = [
+        "avatar_text"
+    ];
 
+    public function getAvatarTextAttr($val, $data)
+    {
+        if (empty($data['avatar']) || !is_file(ROOT_PATH . 'public' . $data['avatar'])) {
+            return cdnurl("/assets/img/avatar.png");
+        }
+        return cdnurl($data['avatar']);
+    }
 }
