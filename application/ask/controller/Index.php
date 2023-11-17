70a536a29de3168140491cabed1756389fb5e117
jack <15014586591@139.com>
2023-11-17 13:27:40 +0800

    666666666666666666666666666666666666666666666666

---
+++ application/ask/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\ask\controller;
+
+use app\common\controller\AskController;
+
+/**
+ * 公共接口
+ */
+class Index extends AskController
+{
+    public function index()
+    {
+        return '没有首页';
+    }
+}
