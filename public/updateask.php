eeba5f4d338aa75abf25319943338755b9f6fb48
jack <15014586591@139.com>
2023-12-06 13:36:13 +0800

    更新hook

--- public/updateask.php
+++ public/updateask.php
@@ -1,12 +1,27 @@
 <?php
-echo "开始更新";
+
 ini_set("max_execution_time", 0);
 ini_set("ignore_user_abort", true);
 chdir("../../../ask.jackr.cn");
-echo `git fetch`;
-echo `git reset --hard origin/master`;
-echo `git pull`;
-echo `npm i`;
-echo `npm run build`;
+function exec_command($command)
+{
+    $process = popen($command, "r");
+    while (!feof($process)) {
+        $output = fread($process, 1024);
+        echo $output;
+    }
+    $exit_code = pclose($process);
+    if ($exit_code == 0) {
+        return true;
+    }
+    echo "{$command}执行失败，退出代码:{$exit_code}";
+    exit;
+}
+
+echo "开始更新";
+exec_command("git fetch");
+exec_command("git reset --hard origin/master");
+exec_command("npm i");
+exec_command("npm run build");
 echo "更新完成";
 
