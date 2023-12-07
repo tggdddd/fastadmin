9f47dbeb3a26db88c20bdde98d866445db425610
jack <15014586591@139.com>
2023-12-07 15:19:44 +0800

    更新hook

--- public/update.sh
+++ public/update.sh
@@ -1,6 +1,16 @@
 echo $1;
 cd $1
+
+if [ -f "update.lock" ];then
+  echo "文件存在"
+  exit
+  else
+    touch "update.lock"
+fi
+
 git fetch
 git reset --hard origin/master
 npm i
 npm run build
+
+rm -f /data/filename
--- public/updateask.php
+++ public/updateask.php
@@ -1,43 +1,12 @@
 <?php
-ini_set("ignore_user_abort", false);
-ini_set("max_execution_time", 120);
-function detect($lock, $seek)
-{
-    if (file_exists($lock) && file_get_contents($lock) != $seek) {
-        echo "请勿多次请求";
-        exit;
-    }
-}
-
-function lock($lock)
-{
-    $seek = rand(0, 99) . "lock";
-    detect($lock, $seek);
-    file_put_contents($lock, $seek);
-    return $seek;
-}
-
-function unlock($lock)
-{
-    @unlink($lock);
-}
-
 function echoi($str)
 {
     echo php_sapi_name() == "cli" ? $str : str_replace("\n", "<br/>", $str);
 }
-
-$lock = __FILE__ . "lock";
-$seek = lock($lock);
-for ($i = 0; $i < 7; $i++) {
-    sleep(rand(3, 10));
-    detect($lock, $seek);
-}
 $path = realpath("../../../ask.jackr.cn");
 $script = __DIR__ . "/update.sh";
 echoi("开始更新\n");
-$command = "sh {$script} {$path} 2>&1";
+$command = "sh {$script} {$path} > update.log";
 echoi("执行命令：$command\n");
-echoi(`$command`);
+`$command`;
 echoi("更新完成\n");
-unlock($lock);
